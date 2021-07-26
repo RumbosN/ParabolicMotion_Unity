11b1b46a798053d3fe3fb2f682be6127d9fb97b5
NorelysR <norelys.rumbos@ludusglobal.com>
2021-07-26 18:26:55 +0200

    feat: Add lightmap to level 1

--- Assets/ParabolicMotion/Scripts/LevelManager.cs
+++ Assets/ParabolicMotion/Scripts/LevelManager.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.SceneManagement;
 
 public class LevelManager : Singleton<LevelManager>
 {
@@ -9,6 +10,7 @@ public class LevelManager : Singleton<LevelManager>
 	[SerializeField] private GameObject _player;
 	[SerializeField] private Transform _playerStartPosition;
 	[SerializeField] private UnityEvent _resetLevelEvent;
+	[SerializeField] private string _nextLevel;
 
 	public void ResetLevel(bool resetPlayer) {
 		_resetLevelEvent.Invoke();
@@ -21,5 +23,10 @@ public class LevelManager : Singleton<LevelManager>
 	public IEnumerator WaitToExecute(int timeToWait, Action action) {
 		yield return new WaitForSeconds(timeToWait);
 		action();
+	}
+
+	public void ChangeToNextLevel() {
+		SceneFadeManager.instance.FadeOut(2);
+		StartCoroutine(WaitToExecute(1, () => SceneManager.LoadScene(_nextLevel)));
 	}
 }
