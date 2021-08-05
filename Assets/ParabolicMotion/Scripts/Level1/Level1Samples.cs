2bcc31d28dd0749f46b309205baa7741d84dff23
NorelysR <norelys.rumbos@ludusglobal.com>
2021-08-05 20:17:00 +0200

    feat: create more case to level 1

--- Assets/ParabolicMotion/Scripts/Level1/Level1Samples.cs
+++ Assets/ParabolicMotion/Scripts/Level1/Level1Samples.cs
@@ -4,6 +4,12 @@ public static class Level1Samples {
 	public static ParabolicVariables[] samples = new [] {
 		new ParabolicVariables(35f, -30f, new Vector3(-60.46f, 0f, -8.04f), 49.3f, 106f),
 		new ParabolicVariables(38f, -30f, new Vector3(-79.2f, 0f, -8.04f), 49.3f, 125f),
+		new ParabolicVariables(40.5f, -30f, new Vector3(-96.22f, 0f, -8.04f), 49.3f, 142f),
+		new ParabolicVariables(45.6f, -30f, new Vector3(-134.28f, 0f, -8.04f), 49.3f, 180f),
+		new ParabolicVariables(49.6f, -30f, new Vector3(-166.9f, 0f, -8.04f), 49.3f, 213f),
+		new ParabolicVariables(52.1f, -30f, new Vector3(-189.24f, 0f, -8.04f), 49.3f, 235f),
+		new ParabolicVariables(54.8f, -30f, new Vector3(-214.29f, 0f, -8.04f), 49.3f, 260f),
+
 		new ParabolicVariables(55f, -45f, new Vector3(-255.86f, 0f, -8.04f), 49.7f, 302.5f)
 	};
 }
