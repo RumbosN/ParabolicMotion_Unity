4d635decf568ea0822b7a1fcf3b9f3854a044d2e
NorelysR <norelys.rumbos@ludusglobal.com>
2021-08-25 15:05:34 -0400

    feat: update level card image in the menu

--- Assets/ParabolicMotion/Scripts/Menu/LevelCard.cs
+++ Assets/ParabolicMotion/Scripts/Menu/LevelCard.cs
@@ -5,18 +5,15 @@ using UnityEngine.UI;
 public class LevelCard : MonoBehaviour
 {
     [SerializeField] private Button _button;
-    [SerializeField] private Image _image;
-    [SerializeField] private Sprite _successLevelImage;
-    [SerializeField] private Sprite _blockedLevelImage;
-    [SerializeField] private Sprite _currentLevelImage;
+    [SerializeField] private GameObject _successLevelImage;
+    [SerializeField] private GameObject _blockedLevelImage;
+    [SerializeField] private GameObject _currentLevelImage;
 
     public void SetStatus(ELevelStatus status) {
 	    _button.interactable = (status != ELevelStatus.BLOCKED);
 
-	    switch (status) {
-            case ELevelStatus.BLOCKED: _image.sprite = _blockedLevelImage; break;
-            case ELevelStatus.CURRENT: _image.sprite = _currentLevelImage; break;
-            case ELevelStatus.SUCCESSFUL: _image.sprite = _successLevelImage; break;
-	    }
+        _successLevelImage.SetActive(status == ELevelStatus.SUCCESSFUL);
+        _blockedLevelImage.SetActive(status == ELevelStatus.BLOCKED);
+        _currentLevelImage.SetActive(status == ELevelStatus.CURRENT);
     }
 }
